329cbded3e568badbb3496530e1f9670eca17aeb
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-08-15 22:23:53 +1200

    pages js files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
 import React from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Link,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
-import NotFoundPage from "./pages/404";
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-            <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
-            </ul>
-          <div className="content">
-          </div>
-        </div>
-        </Router>
-    );
+import SubmitArticle from "./pages/Submit-Article";
+//import NotFoundPage from "./pages/404";
+import "./index.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
+      </div>
+      <ul className="header">
+        <li><Link exact to="/">Home</Link></li>
+        <li><Link to="/SEPractice">Select the Practice</Link></li>
+        <li><Link to="/SubmitArticle">Submit an Article</Link></li>
+      </ul>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/SEPractice" element={<SEPractice />} />
+          <Route path="/SubmitArticle" element={<SubmitArticle />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
- 
+
 export default App;
